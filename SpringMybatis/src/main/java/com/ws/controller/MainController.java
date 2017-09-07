41b9467d5df622384f3c81be87035595c1947e50
SnackMen <vipkia@sina.cn>
2017-09-07 15:34:53 +0800

    添加日志功能

--- SpringMybatis/src/main/java/com/ws/controller/MainController.java
+++ SpringMybatis/src/main/java/com/ws/controller/MainController.java
@@ -1,5 +1,7 @@
 package com.ws.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,13 +10,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/user")
 public class MainController {
 
+    private Logger logger = LoggerFactory.getLogger(MainController.class);
+
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String login(){
+        logger.debug("访问/login方法");
         return "login";
     }
 
     @RequestMapping(value = "/reg", method = RequestMethod.GET)
     public String reg(){
+        logger.debug("访问/login方法");
         return "register";
     }
 
